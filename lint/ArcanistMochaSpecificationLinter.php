f6f55c8ef56547e95db2fd9058f30b08cda989ab
Piotr Kozubek <piotrkozubek@gmail.com>
2018-02-28 13:10:08 +0100

    Updated linter so it will warn about using .has in test instead of throwing error

--- lint/ArcanistMochaSpecificationLinter.php
+++ lint/ArcanistMochaSpecificationLinter.php
@@ -5,6 +5,7 @@
 final class ArcanistMochaSpecificationLinter extends ArcanistLinter {
   const LINT_ONLY_DIRECTIVE = 0;
   const LINT_NO_MATCHER = 1;
+  const LINT_HAS_METHOD_WARNING = 1;
 
   public function getLinterName() {
     return 'MochaSpecs';
@@ -12,14 +13,16 @@ final class ArcanistMochaSpecificationLinter extends ArcanistLinter {
 
   public function getLintSeverityMap() {
     return array(
-      self::LINT_ONLY_DIRECTIVE => ArcanistLintSeverity::SEVERITY_ERROR
+      self::LINT_ONLY_DIRECTIVE => ArcanistLintSeverity::SEVERITY_ERROR,
+      self::LINT_HAS_METHOD_WARNING => ArcanistLintSeverity::SEVERITY_ERROR
     );
   }
 
   public function getLintNameMap() {
     return array(
       self::LINT_ONLY_DIRECTIVE => '"only" directive left in Mocha specification',
-      self::LINT_NO_MATCHER => 'invalid Chai assertion: matcher (e.g. `equal`) needs to come last'
+      self::LINT_NO_MATCHER => 'invalid Chai assertion: matcher (e.g. `equal`) needs to come last',
+      self::LINT_HAS_METHOD_WARNING => 'invalid Chai assertion: matcher (e.g. `has`) needs to come last'
     );
   }
 
@@ -35,7 +38,7 @@ final class ArcanistMochaSpecificationLinter extends ArcanistLinter {
     $lines = phutil_split_lines($this->getData($path), false);
 
     // 'at' is also in this list, but it is Backbone.Colllection method as well
-    $chaiLanguageChain = join('|', array('be', 'been', 'is', 'and', 'has', 'have',
+    $chaiLanguageChain = join('|', array('be', 'been', 'is', 'and', 'have',
       'with', 'that', 'of', 'same', 'not'));
 
     foreach ($lines as $lineno => $line) {
@@ -52,6 +55,12 @@ final class ArcanistMochaSpecificationLinter extends ArcanistLinter {
           0,
           self::LINT_NO_MATCHER,
           "This assertion is incomplete and will succeed for any input");
+      } else if (preg_match("@\.(has)( |\()@", $line)) {
+        $this->raiseLintAtLine(
+          $lineno + 1,
+          0,
+          self::LINT_HAS_METHOD_WARNING,
+          "This assertion is incomplete and it might succeed for any input, but it might be throwed when using Immutable.has in test");
       }
     }
   }
